984be33af54ba505bf47783b1babd91db9a26df6
hehe1111 <2908749709@qq.com>
2020-11-30 21:59:45 +0800

    refactor: 突出主流程

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -4,7 +4,27 @@ import { buildUrl, combineURL, isAbsoluteURL } from '../helpers/url'
 import flattenHeaders from '../helpers/flatternHeaders'
 import transform from './transform'
 
-const transformUrl = (config: AxiosRequestConfig): string => {
+export default (config: AxiosRequestConfig): AxiosPromise => {
+  throwIfCancellationRequested(config)
+  processConfig(config)
+  return xhr(config).then(res => _transformResponseData(res))
+}
+
+/* === 工具函数 === */
+
+function throwIfCancellationRequested(config: AxiosRequestConfig): void {
+  if (config.cancelToken) {
+    config.cancelToken.throwIfRequested()
+  }
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+  config.data = transform(config.data, config.headers, config.transformRequest)
+  config.headers = flattenHeaders(config.headers, config.method!)
+}
+
+function transformUrl(config: AxiosRequestConfig): string {
   let { baseURL, url, params, paramsSerializer } = config
   if (baseURL && !isAbsoluteURL(url!)) {
     url = combineURL(baseURL, url)
@@ -12,25 +32,7 @@ const transformUrl = (config: AxiosRequestConfig): string => {
   return buildUrl({ url: url!, params, paramsSerializer })
 }
 
-const processConfig = (config: AxiosRequestConfig): void => {
-  config.url = transformUrl(config)
-  config.data = transform(config.data, config.headers, config.transformRequest)
-  config.headers = flattenHeaders(config.headers, config.method!)
-}
-
-const _transformResponseData = (res: AxiosResponse): AxiosResponse => {
+function _transformResponseData(res: AxiosResponse): AxiosResponse {
   res.data = transform(res.data, res.headers, res.config.transformResponse)
   return res
-}
-
-export default (config: AxiosRequestConfig): AxiosPromise => {
-  throwIfCancellationRequested(config)
-  processConfig(config)
-  return xhr(config).then(res => _transformResponseData(res))
-}
-
-function throwIfCancellationRequested(config: AxiosRequestConfig): void {
-  if (config.cancelToken) {
-    config.cancelToken.throwIfRequested()
-  }
 }

aa1557cd31d2e5a0ca61e0e46d7981c1b825ed93
hehe1111 <2908749709@qq.com>
2020-12-01 19:40:50 +0800

    test: 添加 src/helpers/error.ts 测试

---
+++ test/helpers/error.spec.ts
@@ -0,0 +1,26 @@
+import { AxiosError, createError } from '../../src/helpers/error'
+import { AxiosRequestConfig, AxiosResponse } from '../../src/types'
+
+describe('src/helpers/error.ts', () => {
+  test('should create an Error with message, config, code, request, response and isAxiosError', () => {
+    const request = new XMLHttpRequest()
+    const config: AxiosRequestConfig = { method: 'post' }
+    const response: AxiosResponse = {
+      data: { foo: 'bar' },
+      status: 200,
+      statusText: 'OK',
+      headers: null,
+      config,
+      request
+    }
+    const error = createError('Boom!', config, 'SOME CODE', request, response)
+    expect(error instanceof Error).toBeTruthy()
+    expect(error instanceof AxiosError).toBeTruthy()
+    expect(error.message).toBe('Boom!')
+    expect(error.config).toBe(config)
+    expect(error.code).toBe('SOME CODE')
+    expect(error.request).toBe(request)
+    expect(error.response).toBe(response)
+    expect(error.isAxiosError).toBeTruthy()
+  })
+})
